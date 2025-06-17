44c43b1841b029d4ec6c916c78963e1c14ce6a8f
svan23 <vansla>
2025-06-16 19:11:54 -0700

    Added package files

--- frontend/src/app/overall_result_page/page.tsx
+++ frontend/src/app/overall_result_page/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation'; // Changed from next/router
 
 export default function OverallResultPage() {
     const router = useRouter();
